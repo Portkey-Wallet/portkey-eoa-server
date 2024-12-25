d20ee5f8c654571956c8659bb1cdfe04d42dd2f6
mixhsnhd <lightning_king100@portkey.finance>
2024-12-25 10:16:45 +0800

    feat: orleans k8s refact

--- src/EoaServer.Silo/Extensions/OrleansHostExtensions.cs
+++ src/EoaServer.Silo/Extensions/OrleansHostExtensions.cs
@@ -8,12 +8,13 @@ using Orleans.Configuration;
 using Orleans.Hosting;
 using Orleans.Providers.MongoDB.Configuration;
 using Orleans.Statistics;
+using Serilog;
 
 namespace EoaServer.Silo.Extensions;
 
 public static class OrleansHostExtensions
 {
-     public static IHostBuilder UseOrleans(this IHostBuilder hostBuilder)
+    public static IHostBuilder UseOrleans(this IHostBuilder hostBuilder)
     {
         var configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
@@ -24,61 +25,151 @@ public static class OrleansHostExtensions
             throw new ArgumentNullException(nameof(configSection), "The OrleansServer node is missing");
         return hostBuilder.UseOrleans(siloBuilder =>
         {
-            //Configure OrleansSnapshot
-            siloBuilder
-                .ConfigureEndpoints(advertisedIP:IPAddress.Parse(configSection.GetValue<string>("AdvertisedIP")),siloPort: configSection.GetValue<int>("SiloPort"), gatewayPort: configSection.GetValue<int>("GatewayPort"), listenOnAnyHostAddress: true)
-                .UseMongoDBClient(configSection.GetValue<string>("MongoDBClient"))
-                .UseMongoDBClustering(options =>
-                {
-                    options.DatabaseName = configSection.GetValue<string>("DataBase");
-                    options.Strategy = MongoDBMembershipStrategy.SingleDocument;
-                })
-                .Configure<JsonGrainStateSerializerOptions>(options => options.ConfigureJsonSerializerSettings =
-                    settings =>
-                    {
-                        settings.NullValueHandling = NullValueHandling.Include;
-                        settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
-                        settings.DefaultValueHandling = DefaultValueHandling.Populate;
-                    })
-                .AddMongoDBGrainStorage("Default",(MongoDBGrainStorageOptions op) =>
-                {
-                    op.CollectionPrefix = "GrainStorage";
-                    op.DatabaseName = configSection.GetValue<string>("DataBase");
-                
-                    var grainIdPrefix = configSection
-                        .GetSection("GrainSpecificIdPrefix").GetChildren().ToDictionary(o => o.Key.ToLower(), o => o.Value);
-                    op.KeyGenerator = id =>
-                    {
-                        var grainType = id.Type.ToString();
-                        if (grainIdPrefix.TryGetValue(grainType, out var prefix))
-                        {
-                            return prefix.StartsWith("GrainReference=000000") ? $"{prefix}+{id.Key}" : prefix;
-                        }
-                        return id.ToString();
-                    };
-                    op.CreateShardKeyForCosmos = configSection.GetValue<bool>("CreateShardKeyForMongoDB", false);
-
-                })
-                .UseMongoDBReminders(options =>
-                {
-                    options.DatabaseName = configSection.GetValue<string>("DataBase");
-                    options.CreateShardKeyForCosmos = false;
-                })
-                .Configure<ClusterOptions>(options =>
-                {
-                    options.ClusterId = configSection.GetValue<string>("ClusterId");
-                    options.ServiceId = configSection.GetValue<string>("ServiceId");
-                })
-               // .AddMemoryGrainStorage("PubSubStore")
-                .UseDashboard(options => {
-                    options.Username = configSection.GetValue<string>("DashboardUserName");
-                    options.Password = configSection.GetValue<string>("DashboardPassword");
-                    options.Host = "*";
-                    options.Port = configSection.GetValue<int>("DashboardPort");
-                    options.HostSelf = true;
-                    options.CounterUpdateIntervalMs = configSection.GetValue<int>("DashboardCounterUpdateIntervalMs");
-                })
-                .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug).AddConsole(); });
+            if (configSection.GetValue<bool>("IsRunningInKubernetes"))
+            {
+                Log.Warning("==Use kubernetes hosting...");
+                UseKubernetesHostClustering(siloBuilder, configSection);
+                Log.Warning("==Use kubernetes hosting end...");
+            }
+            else
+            {
+                Log.Warning("==Use docker hosting...");
+                UseDockerHostClustering(siloBuilder, configSection);
+                Log.Warning("==Use docker hosting end...");
+            }
         });
+    }
+
+    private static void UseKubernetesHostClustering(ISiloBuilder siloBuilder, IConfigurationSection configSection)
+    {
+        Log.Warning("==Configuration");
+        Log.Warning("==  POD_IP: {0}", Environment.GetEnvironmentVariable("POD_IP"));
+        Log.Warning("==  SiloPort: {0}", configSection.GetValue<int>("SiloPort"));
+        Log.Warning("==  GatewayPort: {0}", configSection.GetValue<int>("GatewayPort"));
+        Log.Warning("==  DatabaseName: {0}", configSection.GetValue<string>("DataBase"));
+        Log.Warning("==  ClusterId: {0}", Environment.GetEnvironmentVariable("ORLEANS_CLUSTER_ID"));
+        Log.Warning("==  ServiceId: {0}", Environment.GetEnvironmentVariable("ORLEANS_SERVICE_ID"));
+        Log.Warning("==Configuration");
+
+        //Configure OrleansSnapshot
+        siloBuilder
+            .ConfigureEndpoints(
+                advertisedIP: IPAddress.Parse(Environment.GetEnvironmentVariable("POD_IP") ?? string.Empty),
+                siloPort: configSection.GetValue<int>("SiloPort"),
+                gatewayPort: configSection.GetValue<int>("GatewayPort"), listenOnAnyHostAddress: true)
+            .UseMongoDBClient(configSection.GetValue<string>("MongoDBClient"))
+            .UseMongoDBClustering(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+            })
+            .Configure<JsonGrainStateSerializerOptions>(options => options.ConfigureJsonSerializerSettings =
+                settings =>
+                {
+                    settings.NullValueHandling = NullValueHandling.Include;
+                    settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+                    settings.DefaultValueHandling = DefaultValueHandling.Populate;
+                })
+            .AddMongoDBGrainStorage("Default", (MongoDBGrainStorageOptions op) =>
+            {
+                op.CollectionPrefix = "GrainStorage";
+                op.DatabaseName = configSection.GetValue<string>("DataBase");
+
+                var grainIdPrefix = configSection
+                    .GetSection("GrainSpecificIdPrefix").GetChildren().ToDictionary(o => o.Key.ToLower(), o => o.Value);
+                op.KeyGenerator = id =>
+                {
+                    var grainType = id.Type.ToString();
+                    if (grainIdPrefix.TryGetValue(grainType, out var prefix))
+                    {
+                        return prefix.StartsWith("GrainReference=000000") ? $"{prefix}+{id.Key}" : prefix;
+                    }
+
+                    return id.ToString();
+                };
+                op.CreateShardKeyForCosmos = configSection.GetValue<bool>("CreateShardKeyForMongoDB", false);
+            })
+            .UseMongoDBReminders(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.CreateShardKeyForCosmos = false;
+            })
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = Environment.GetEnvironmentVariable("ORLEANS_CLUSTER_ID");
+                options.ServiceId = Environment.GetEnvironmentVariable("ORLEANS_SERVICE_ID");
+            })
+            // .AddMemoryGrainStorage("PubSubStore")
+            .UseDashboard(options =>
+            {
+                options.Username = configSection.GetValue<string>("DashboardUserName");
+                options.Password = configSection.GetValue<string>("DashboardPassword");
+                options.Host = "*";
+                options.Port = configSection.GetValue<int>("DashboardPort");
+                options.HostSelf = true;
+                options.CounterUpdateIntervalMs = configSection.GetValue<int>("DashboardCounterUpdateIntervalMs");
+            })
+            .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug).AddConsole(); });
+    }
+
+    private static void UseDockerHostClustering(ISiloBuilder siloBuilder, IConfigurationSection configSection)
+    {
+        siloBuilder
+            .ConfigureEndpoints(advertisedIP: IPAddress.Parse(configSection.GetValue<string>("AdvertisedIP")),
+                siloPort: configSection.GetValue<int>("SiloPort"),
+                gatewayPort: configSection.GetValue<int>("GatewayPort"), listenOnAnyHostAddress: true)
+            .UseMongoDBClient(configSection.GetValue<string>("MongoDBClient"))
+            .UseMongoDBClustering(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+            })
+            .Configure<JsonGrainStateSerializerOptions>(options => options.ConfigureJsonSerializerSettings =
+                settings =>
+                {
+                    settings.NullValueHandling = NullValueHandling.Include;
+                    settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+                    settings.DefaultValueHandling = DefaultValueHandling.Populate;
+                })
+            .AddMongoDBGrainStorage("Default", (MongoDBGrainStorageOptions op) =>
+            {
+                op.CollectionPrefix = "GrainStorage";
+                op.DatabaseName = configSection.GetValue<string>("DataBase");
+
+                var grainIdPrefix = configSection
+                    .GetSection("GrainSpecificIdPrefix").GetChildren().ToDictionary(o => o.Key.ToLower(), o => o.Value);
+                op.KeyGenerator = id =>
+                {
+                    var grainType = id.Type.ToString();
+                    if (grainIdPrefix.TryGetValue(grainType, out var prefix))
+                    {
+                        return prefix.StartsWith("GrainReference=000000") ? $"{prefix}+{id.Key}" : prefix;
+                    }
+
+                    return id.ToString();
+                };
+                op.CreateShardKeyForCosmos = configSection.GetValue<bool>("CreateShardKeyForMongoDB", false);
+            })
+            .UseMongoDBReminders(options =>
+            {
+                options.DatabaseName = configSection.GetValue<string>("DataBase");
+                options.CreateShardKeyForCosmos = false;
+            })
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = configSection.GetValue<string>("ClusterId");
+                options.ServiceId = configSection.GetValue<string>("ServiceId");
+            })
+            // .AddMemoryGrainStorage("PubSubStore")
+            .UseDashboard(options =>
+            {
+                options.Username = configSection.GetValue<string>("DashboardUserName");
+                options.Password = configSection.GetValue<string>("DashboardPassword");
+                options.Host = "*";
+                options.Port = configSection.GetValue<int>("DashboardPort");
+                options.HostSelf = true;
+                options.CounterUpdateIntervalMs = configSection.GetValue<int>("DashboardCounterUpdateIntervalMs");
+            })
+            .ConfigureLogging(logging => { logging.SetMinimumLevel(LogLevel.Debug).AddConsole(); });
     }
 }
